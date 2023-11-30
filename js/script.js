ad68a60ceecd3413a2e62d4614faf1dd6629bdbe
Xennan <darquantumgames@gmail.com>
2023-12-01 01:00:12 +0200

    practice 1

--- js/script.js
+++ js/script.js
@@ -1,21 +1,4 @@
 "use strict";
 
 
-
-let incr = 10,
-    decr = 10;
-
-//++incr;
-//--decr;
-
-console.log(incr++);
-console.log(decr--);
-
-console.log(5%2);
-
-console.log(2*4 === 8);
-
-const isChecked = false,
-    isClose = false;
-
-console.log(isChecked || !isClose);
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
