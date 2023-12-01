afaab1f9e191bd307f4436dd574f4c2e12f1469e
Xennan <darquantumgames@gmail.com>
2023-12-01 22:47:28 +0200

    practice 1 p2

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@
 
 
 const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt("Какой ваш последний просмотренный фильм? ");
+    b = +prompt("Сколько баллов ему поставите? ");
+    c = prompt("Какой ваш последний просмотренный фильм? ");
+    d = +prompt("Сколько баллов ему поставите? ");
+
+personalMovieDB.movies[a] = b
+personalMovieDB.movies[c] = d
+
+console.log(personalMovieDB)
