b8a7a23fc8ef3d17fb95c753cc6fa9248556feec
Xennan <darquantumgames@gmail.com>
2023-12-03 01:25:43 +0200

    my christmas tree

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
-"use strict";
-
-
-
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
-
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
-
-let num = 50;
-
-for (let i  = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
+for (let i = 0; i < 3; i++) {
     console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
 }
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+let result = "";
+const length = 12;
+let prob = "          ";
+
+for (let i = 1; i < length; i += 2) {
+    result += prob;
+    for (j = 0; j < i; j++) {
+        result += "*";
+    }
+    result += "\n"
+    
+    prob = prob.slice(1);
+}
+
+console.log(result);
