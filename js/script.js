41d4e7060950009bd08e4d97a1c8ad342a05c5cd
Xennan <darquantumgames@gmail.com>
2023-12-03 02:03:48 +0200

    cycles (additional lesson)

--- js/script.js
+++ js/script.js
@@ -13,17 +13,26 @@ for (let i = 0; i < 3; i++) {
 // ******
 
 let result = "";
-const length = 12;
-let prob = "          ";
+const length = 7;
 
-for (let i = 1; i < length; i += 2) {
-    result += prob;
+for (let i = 1; i < length; i += 1) {
+    
     for (j = 0; j < i; j++) {
         result += "*";
     }
     result += "\n"
     
-    prob = prob.slice(1);
 }
 
 console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) break first;
+            console.log(`Third level: ${k}`);
+        }
+    }
+}
