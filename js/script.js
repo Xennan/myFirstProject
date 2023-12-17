b0a8ce8a5ed676ed7f23295afc2aac340caf554d
Xennan <darquantumgames@gmail.com>
2023-12-18 01:12:53 +0200

    practice No 10, fckng objects and arrays

--- js/script.js
+++ js/script.js
@@ -1,96 +1,41 @@
 "use strict";
+// НИ ОДНО РЕШЕНИЕ НЕ МОЁ, Я НИХРЕНИЩЕ НЕ ПОНЯЛ
 
-// let a = 5,
-//     b = a;
-
-// b = b + 5;
-
-// console.log(b);
-// console.log(a);
-
-// const obj = {
-//     a: 5,
-//     b: 1
-// };
-
-// const copy = obj; //Ссылка
-
-// copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
-
-function copy(mainObj) {
-    let objCopy = {};
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function(plan) {
+        let {age} = plan;
+        let {languages} = plan.skills;
+        let str = `Мне ${age} и я владею языками: `;
+        
+        languages.forEach(function(lang) {
+            str += `${lang.toUpperCase()} `;
+        });
+        return str;
     }
-
-    return objCopy;
 };
 
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
+function showExperience(plan) {
+    const d = plan.skills.exp;
+    return d;
+}
+
+function showProgrammingLangs(plan) {
+    let str = '';
+    const {programmingLangs} = plan.skills;
+    
+    for (let key in programmingLangs) {
+        str += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+        
     }
+    return str;
 }
-
-const newNumbers = copy(numbers); 
-
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-
-// console.log(newNumbers);
-// console.log(numbers);
-
-// const add = {
-//     d: 17,
-//     e: 20
-// };
-
-// const clone = Object.assign({}, add);
-
-// clone.d = 20;ve
-
-// console.log(add);
-// console.log(clone);
-
-const oldArray = ['a','b','c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'sdafasfs';
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['worldpress', 'livejournal', 'blogger'];
-      internet = [...video, ...blogs, 'vk', 'facebook'];
-console.log(internet);
-
-function log(a, b, c,) {
-    console.log(a);
-    console.log(c);
-    console.log(b);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-const array = ['a', 'b'];
-
-const newAarray = [...array];
-console.log(newAarray);
-
-const q = {
-    one: 1,
-    two: 2
-};
-
-const newObj = {...q};
-console.log(q);
-
