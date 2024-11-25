1e408f99d1c5b4e9f9ceb23a974950e48d3963c1
Xennan <darquantumgames@gmail.com>
2024-11-25 04:31:12 +0200

    76. Classes (ES6)

--- src/js/script.js
+++ src/js/script.js
@@ -1,57 +1,26 @@
 'use strict';
 
-// function showThis(a, b) {
-//     console.log(this);
-//     function sum() {
-//         console.log(this);
-//         return a + b;
-//     }
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
 
-//     console.log(sum());
-// }
-// showThis(4, 5);
-
-// 1) с "use strict"; значиние будет undefined, но если его не будет то значением станет объект window
-
-// const obj = {
-//     a: 20,
-//     b: 15,
-//     sum: function() {
-//         function shout() {
-//             console.log(this);
-//         }
-//         shout();
-//     }
-// };
-// obj.sum();
-
-// 2) Контекст у методов объекта это сам объект
-
-// function User(name, id) {
-//     this.name = name;
-//     this.id = id;
-//     this.human = human;
-// }
-// let ivan = new User('Ivan', 23);
-
-// 3) this в конструкторах и классах - это новый экземпляр объекта
-
-function sayName(surname) {
-    console.log(this);
-    console.log(this.name + surname);
+    calcArea() {
+        return this.height * this.width;
+    }
 }
 
-const user = {
-    name: 'John'
-};
-
-sayName.call(user, 'Smith');
-sayName.apply(user, ['Smith']);
-
-function count(num) {
-    return this*num;
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
 }
 
-const double = count.bind(2);
-console.log(double(3));
-console.log(double(333));
+const square = new Rectangle(10, 10);
+const long = new Rectangle(20, 100);
+
+console.log(square.calcArea());
+console.log(long.calcArea());
