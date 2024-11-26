c603a4ea7e3c77b92487016c5af51c418e90f706
Xennan <darquantumgames@gmail.com>
2024-11-26 02:51:44 +0200

    79. Rest operator and default values (ES6)

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,11 @@
-'use strict';
-
-class Rectangle {
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
-    }
-
-    calcArea() {
-        return this.height * this.width;
-    }
+const log = function(a, b, ...rest) {
+    console.log(a, b, rest);
 }
 
-class ColoredRectangleWithText extends Rectangle {
-    constructor(height, width, text, bgColor) {
-        super(height, width);
-        this.text = text;
-        this.bgColor = bgColor;
-    }
+log('basic', 'rest', 'operator', 'usage');
+
+function calcOrDouble(number, basis = 2) {
+    console.log(number * basis);
 }
 
-const square = new Rectangle(10, 10);
-const long = new Rectangle(20, 100);
-
-console.log(square.calcArea());
-console.log(long.calcArea());
+calcOrDouble(3);
